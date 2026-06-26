e95fcbe4344f3dc8a8f4439269220be8e0a818f7
MiracleAgha <aghamiracle123@gmail.com>
2026-06-26 12:38:00 +0100

    cors a,llow all origin

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,33 +1,8 @@
 const cors = require('cors');
-const env = require('./env');
-
-const allowedOrigins = [
-  env.CLIENT_URL,
-  env.STUDENT_CLIENT_URL,
-];
-
-// In development, also allow common local dev ports
-if (env.isDev()) {
-  allowedOrigins.push(
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:5175',
-    'http://127.0.0.1:5173',
-    'http://127.0.0.1:5174',
-  );
-}
 
 const corsOptions = {
   origin(origin, callback) {
-    // Allow requests with no origin (mobile apps, curl, Postman)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    return callback(new Error(`Origin ${origin} not allowed by CORS`));
+    callback(null, true);
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
@@ -37,3 +12,4 @@ const corsOptions = {
 };
 
 module.exports = { corsMiddleware: cors(corsOptions), corsOptions };
+
